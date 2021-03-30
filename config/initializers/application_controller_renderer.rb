acf7daaa477d48bee13971546c95cf2a4864f23e
中元寺　裕也 <chugen@chuugenjiyuuya-no-MacBook-Pro.local>
2021-03-30 17:36:31 +0900

    camp_mesi_sampke

---
+++ config/initializers/application_controller_renderer.rb
@@ -0,0 +1,10 @@
+# Be sure to restart your server when you modify this file.
+
+# ActiveSupport::Reloader.to_prepare do
+#   ApplicationController.renderer.defaults.merge!(
+#     http_host: 'example.org',
+#     https: false
+#   )
+# end
+
+Refile.secret_key = '6fcc531b82dade169eb82b32837f6c42549a1e07c8f4553b020d6ebb5ea173a6e3971907df2e851b5f4ac955fc888ff083c0f9f1f1b2922d641bf9ecb1dea00b'
